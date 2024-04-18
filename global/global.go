c85e33958629fb591a686139584269c832c0b304
Junhiee <secret.kokocyber@gmail.com>
2024-04-18 09:26:05 +0800

    add: global G_ZLOG

--- global/global.go
+++ global/global.go
@@ -1,7 +1,11 @@
 package global
 
-import datebase "git.virjar.com/Junhiee/anilismei/database"
+import (
+	datebase "git.virjar.com/Junhiee/anilismei/database"
+	"go.uber.org/zap"
+)
 
 var (
 	G_QRY *datebase.Store
+	ZLOG  *zap.Logger
 )
