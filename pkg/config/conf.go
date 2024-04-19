9c732bb19244fd82b7b0b8f00da8ba587b528788
Junhiee <secret.kokocyber@gmail.com>
2024-04-19 15:18:32 +0800

    add: pkg/config/conf.go

---
+++ pkg/config/conf.go
@@ -0,0 +1,35 @@
+package config
+
+import (
+	"git.virjar.com/Junhiee/anilismei/pkg/log"
+	"github.com/spf13/viper"
+	"go.uber.org/zap"
+)
+
+type DateBase struct{}
+
+type Server struct{}
+
+type Logger struct{}
+
+type Config struct {
+	*DateBase
+	*Server
+}
+
+var cfg Config
+
+func InitConfig() {
+	viper.SetConfigName("conf")      // 文件名
+	viper.SetConfigType("toml")      // 文件类型
+	viper.AddConfigPath("./configs") // 配置文件路径
+	err := viper.ReadInConfig()
+	if err != nil {
+		log.ZLOG.Panic("Viper config init err", zap.Error(err))
+	}
+
+	err = viper.Unmarshal(&cfg)
+	if err != nil {
+		log.ZLOG.Panic("Viper unmarshal faild", zap.Error(err))
+	}
+}
