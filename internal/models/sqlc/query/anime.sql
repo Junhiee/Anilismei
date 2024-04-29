97547ba88d1820b3da15a46a1f910d3c10f9075f
Junhiee <secret.kokocyber@gmail.com>
2024-04-29 14:14:42 +0800

    add: 查询Anime表的各种sql

--- internal/models/sqlc/query/anime.sql
+++ internal/models/sqlc/query/anime.sql
@@ -1,12 +1,32 @@
--- name: GetAnime :one
+-- name: GetAnimeByID :one
 SELECT * FROM animations
 WHERE anime_id = ?;
 
+-- name: GetAnimesByIDs :many
+SELECT * FROM animations
+WHERE anime_id IN (sqlc.slice('ids'))
+ORDER BY update_time DESC;
+
 -- name: GetListAnimes :many
 SELECT * FROM animations
-ORDER BY update_time
+ORDER BY update_time DESC
 LIMIT ? OFFSET ?;
 
--- name: AddAnime :exec
-INSERT INTO animations(anime_id, genre_id, studio_id, title, country, image_url, evaluate, update_time, release_date, anime_status, rating)
-VALUES (?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?);
+-- name: GetAnimesByCountry :many
+SELECT * FROM animations
+WHERE country = ?
+ORDER BY update_time DESC
+LIMIT ? OFFSET ?;
+
+-- name: GetAnimesByRelease :many
+SELECT * FROM animations
+WHERE release_date = ?
+ORDER BY update_time DESC;
+
+-- name: GetAnimesByType :many
+SELECT * FROM animation_genres
+WHERE genre_id = ?;
+
+-- name: InsertAnime :exec
+INSERT INTO animations(anime_id, genre_id, studio_id, title, country, image_url, evaluate, update_time,
+release_date, anime_status, rating) VALUES (?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?);
